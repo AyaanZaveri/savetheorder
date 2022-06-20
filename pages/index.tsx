a1e5c4febd10cd6c5184e7bab556d7c11bd6809e
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-06-20 19:25:17 -0400

    Fixing logo proportions.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { HiSearch } from "react-icons/hi";
 
 const styles = {
-  card: "grid h-36 w-72 place-items-center rounded-xl border border-slate-200 shadow-xl shadow-slate-50 ring-slate-200 focus:ring focus:border-slate-400 active:bg-slate-100 outline-none transition duration-500 ease-in-out hover:cursor-pointer hover:border-slate-300 hover:shadow-slate-100",
+  card: "flex h-36 w-72 items-center justify-center rounded-xl border border-slate-200 shadow-xl shadow-slate-50 ring-slate-200 focus:ring focus:border-slate-400 active:bg-slate-100 outline-none transition duration-500 ease-in-out hover:cursor-pointer hover:border-slate-300 hover:shadow-slate-100",
 };
 
 const Home: NextPage = () => {
@@ -47,11 +47,11 @@ const Home: NextPage = () => {
 
   return (
     <div className="mt-12 flex flex-col items-center justify-center font-['Inter']">
-      <div className="inline-flex w-full items-center justify-center relative">
-        <HiSearch className="pointer-events-none h-4 w-4 left-8 relative transform text-slate-500" />
+      <div className="relative inline-flex w-full items-center justify-center">
+        <HiSearch className="pointer-events-none relative left-8 h-4 w-4 transform text-slate-500" />
         <input
           type="text"
-          className="w-1/3 rounded-lg pl-10 rounded-tr-3xl border border-slate-200 text-slate-500 px-4 py-2 shadow-xl shadow-slate-50 outline-none ring-slate-200 transition duration-500 ease-in-out hover:border-slate-300 hover:shadow-slate-100 focus:border-slate-400 focus:ring active:bg-slate-100 active:ring"
+          className="w-1/3 rounded-lg rounded-tr-3xl border border-slate-200 px-4 py-2 pl-10 text-slate-500 shadow-xl shadow-slate-50 outline-none ring-slate-200 transition duration-500 ease-in-out hover:border-slate-300 hover:shadow-slate-100 focus:border-slate-400 focus:ring active:bg-slate-100 active:ring"
           placeholder="Search your restaurants..."
           onChange={(e) => setSearch(e.target.value)}
         />
@@ -59,15 +59,17 @@ const Home: NextPage = () => {
       <div className="mt-12 flex w-9/12 flex-row flex-wrap items-center justify-center gap-3">
         {restaurants
           .filter((restaurant) =>
-            restaurant.restaurant.toLowerCase().includes(search)
+            restaurant.restaurant.includes(search.toLowerCase())
           )
           .map((restaurant) => (
             <button className={styles.card}>
-              <img
-                src={restaurant.logo}
-                alt=""
-                className="pointer-events-none h-32 p-6"
-              />
+              <div className="w-2/3 flex items-center justify-center">
+                <img
+                  src={restaurant.logo}
+                  alt=""
+                  className="pointer-events-none h-32 p-5"
+                />
+              </div>
             </button>
           ))}
       </div>
