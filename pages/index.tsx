9005002f49e380c1f792b672f3dd8b3f176c844d
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-06-20 18:54:44 -0400

    Search

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,13 +47,15 @@ const Home: NextPage = () => {
 
   return (
     <div className="mt-12 flex flex-col items-center justify-center font-['Inter']">
+      <div className="inline-flex w-full items-center justify-center relative">
+        <HiSearch className="pointer-events-none h-4 w-4 left-8 relative transform text-slate-500" />
         <input
           type="text"
-          className="w-1/3 rounded-lg rounded-tr-3xl border border-slate-200 px-4 py-2 shadow-xl shadow-slate-50 outline-none ring-slate-200 transition duration-500 ease-in-out hover:border-slate-300 hover:shadow-slate-100 focus:border-slate-400 focus:ring active:bg-slate-100 active:ring"
+          className="w-1/3 rounded-lg pl-10 rounded-tr-3xl border border-slate-200 text-slate-600 px-4 py-2 shadow-xl shadow-slate-50 outline-none ring-slate-200 transition duration-500 ease-in-out hover:border-slate-300 hover:shadow-slate-100 focus:border-slate-400 focus:ring active:bg-slate-100 active:ring"
           placeholder="Search your restaurants..."
           onChange={(e) => setSearch(e.target.value)}
         />
-        
+      </div>
       <div className="mt-12 flex w-9/12 flex-row flex-wrap items-center justify-center gap-3">
         {restaurants
           .filter((restaurant) =>
