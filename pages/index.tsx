68d638e9a23164b35fcae611a44e9d7a2349687f
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-06-20 19:43:43 -0400

    Radius

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,7 @@ const Home: NextPage = () => {
         <HiSearch className="pointer-events-none relative left-8 h-4 w-4 transform text-slate-500" />
         <input
           type="text"
-          className="w-1/3 rounded-lg rounded-tr-3xl border border-slate-200 px-4 py-2 pl-10 text-slate-500 shadow-xl shadow-slate-50 outline-none ring-slate-200 transition duration-500 ease-in-out hover:border-slate-300 hover:shadow-slate-100 focus:border-slate-400 focus:ring active:bg-slate-100 active:ring"
+          className="w-1/3 rounded-xl border border-slate-200 px-4 py-2 pl-10 text-slate-500 shadow-xl shadow-slate-50 outline-none ring-slate-200 transition duration-500 ease-in-out hover:border-slate-300 hover:shadow-slate-100 focus:border-slate-400 focus:ring active:bg-slate-100 active:ring"
           placeholder="Search your restaurants..."
           onChange={(e) => setSearch(e.target.value)}
         />
@@ -59,7 +59,7 @@ const Home: NextPage = () => {
       <div className="mt-12 flex w-9/12 flex-row flex-wrap items-center justify-center gap-3">
         {restaurants
           .filter((restaurant) =>
-            restaurant.restaurant.includes(search.toLowerCase())
+            restaurant.restaurant.toLowerCase().includes(search.trim().toLowerCase())
           )
           .map((restaurant) => (
             <button className={styles.card}>
